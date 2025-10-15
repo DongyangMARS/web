32fff0c1185ee60d9dd68101188296211c8ebc66
JiHyeon-9 <jipong9296@naver.com>
2025-10-15 17:03:26 +0900

    ,,

--- src/components/VideoSection.js
+++ src/components/VideoSection.js
@@ -1,18 +1,14 @@
 import '../styles/VideoSection.css';
 import '../styles/color.css';
-<<<<<<< Updated upstream
-=======
 import gifResearch from './gifResearch.gif';
 import gifGameplay from './gifGameplay.gif';
 
->>>>>>> Stashed changes
 function VideoSection() {
   return (
     <section className="video-section" id="video">
       <h2 className="video-title">시연 영상</h2>
       <div className="video-container">
         <div className="video-box">
-<<<<<<< Updated upstream
           <iframe
             className="youtube"
             src="https://youtu.be/XGxIE1hr0w4?si=MAouibA8ROmwJ5gH"
@@ -21,17 +17,14 @@ function VideoSection() {
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           ></iframe>
-=======
           <img
             src={gifResearch}
             title="시연영상1"
             alt="시연영상1"
             className="video-gif"
           />
->>>>>>> Stashed changes
         </div>
         <div className="video-box">
-<<<<<<< Updated upstream
           <iframe
             className="youtube"
             src="https://youtu.be/dQw4w9WgXcQ?si=GbXX6k-8L4dj2feS"
@@ -40,14 +33,12 @@ function VideoSection() {
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           ></iframe>
-=======
           <img
             src={gifGameplay}
             title="시연영상2"
             alt="시연영상2"
             className="video-gif"
           />
->>>>>>> Stashed changes
         </div>
       </div>
     </section>
